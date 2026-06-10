69957d82b17b48c15c1069addd097c93575d5aaf
voletyabhinav <voletyabhinav@gmail.com>
2026-06-10 11:46:16 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable= 0.12345
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "Born in the U.S.A"
+print(type(variable))
+
+variable = [0,4,5,2,3]
+print(type(variable))
