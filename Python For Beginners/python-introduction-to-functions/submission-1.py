1f05be8dcf8df95fe03fa35efdc7436a77815892
voletyabhinav <voletyabhinav@gmail.com>
2026-06-29 14:22:40 -0500

    Add: python-introduction-to-functions - submission-1

---
+++ Python For Beginners/python-introduction-to-functions/submission-1.py
@@ -0,0 +1,11 @@
+def greet():
+    print("Hello, World!")
+greet() # This will print "Hello, world!"
+
+
+# don't modify below this line
+
+def say_goodbye():
+    print("Goodbye, World!")
+
+say_goodbye()
